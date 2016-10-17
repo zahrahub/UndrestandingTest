61d103281047a158a352b87b5383e24862e3cf86
zahrahub <zahra.mollanoori@gmail.com>
2016-10-17 15:20:16 -0400

    this is second vesion

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,12 @@ package welcome;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		int x = 10;
+		if(x > 5){
+			System.out.println("greater");
+		}else{
+			System.out.println("less than");
+		}
 	}
 
 }
